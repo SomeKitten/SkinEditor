5491e7324a7663bed59175cc9487930b3aef79a7
SomeKitten <blueredkitten@gmail.com>
2021-09-27 20:44:04 -0400

    Fixed CNAME issues

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { camera, renderer, scene } from './render'
 import { camOrbit, lookAt } from './util'
 import './listeners'
 
+console.log('TEST!')
+
 lookAt(camera, camOrbit)
 
 const animate = function () {
